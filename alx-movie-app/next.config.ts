80069fd96d622b156c728c247fc8f14e0b20611d
feyruza dawud <feyruza.dawud-ug@aau.edu.et>
2025-08-22 18:21:04 +0300

    Added the confguration

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,18 +1,17 @@
 import withPWAInit from "@ducanh2912/next-pwa";
 
 /** @type {import('next').NextConfig} */
-
 const withPWA = withPWAInit({
-    dest: 'public'
-})
+  dest: "public",
+});
 
-const nextConfig={
-    reactStringMode: true,
-    images: {
-        domains: ['m.media-amazon.com'],
-    },
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["m.media-amazon.com"],
+  },
 };
 
 export default withPWA({
-    ..nextConfig
-})
+  ...nextConfig,
+});
